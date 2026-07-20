99486535b83096072b6519ce1fd45752bf383564
Meiirzhan Muratbekuly <smilexah@inbox.ru>
2026-07-20 11:42:46 +0500

    initialize Spring Boot project with Gradle setup and basic application structure

---
+++ src/test/java/ai/lab/weeklyreport/WeeklyreportApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.lab.weeklyreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyreportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
